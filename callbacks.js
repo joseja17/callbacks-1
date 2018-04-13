746f3b4400f7494ad70010b19ec7b2f0db5c684e
José Jaime Carvajal Calixto <joseja_17@hotmail.com>
2018-04-13 15:32:14 -0600

    adding my responses

--- callbacks.js
+++ callbacks.js
@@ -3,19 +3,31 @@
 
 //////////////////PROBLEM 1////////////////////
 
-//Write a function called stringItOut that takes in 
-//an array, sorts it from greatest to least, and returns 
-//the array converted into a string using the native 
-//array toString method.
+// Write a function called stringItOut that takes in 
+// an array, sorts it from greatest to least, and returns 
+// the array converted into a string using the native 
+// array toString method.
+// var array = [
+//   "hello", "beautiful", "world"
+// ]
 
-  //Code here
+ function stringItOut(arr){
+  arr.sort();
+ arr.sort(function(a, b) {
+   return a-b
+ })
+ 
+ arr.reverse()
+ return arr.toString();
+ }
+//  stringItOut(array)
 
 
 //////////////////PROBLEM 2////////////////////
 
-//Given the functions below: invoke the function math
-//passing in complex, 2, 3 as parameters, and save the result
-//to a variable called mathResult.
+// Given the functions below: invoke the function math
+// passing in complex, 2, 3 as parameters, and save the result
+// to a variable called mathResult.
 
   function math(cb, num1, num2){
     return cb(num1, num2)
@@ -25,4 +37,4 @@
     return Math.sin((Math.log(num1) + Math.tan(num2) * 100))
   }
 
-  //Code here
+var mathResult = math(complex,2, 3)
